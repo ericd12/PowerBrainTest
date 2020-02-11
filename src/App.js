055d41c13527aca7281546982eaa56d7242a6f75
SamsonPit12 <edressler6@gmail.com>
2020-02-11 16:57:35 -0500

    Revert "Updated multiple Element components"
    This reverts commit a178c39070a82739fb3f4ba674c839eaf68013d3.

--- src/App.js
+++ src/App.js
@@ -3,23 +3,22 @@ import {BrowserRouter as Router, Route} from 'react-router-dom';
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 
-import NavbarMain from "./components/navbar.component"
-import ElementsList from "./components/elements-list.component";
-import CreateElement from "./components/create-element.component";
-// import ManageElement from "./components/edit-element.component";
-// import CreateUser from "./components/create-user.component";
+import Navbar from "./components/navbar.component"
+import ExercisesList from "./components/exercises-list.component";
+import EditExercise from "./components/edit-exercise.component";
+import CreateExercise from "./components/create-exercise.component";
+import CreateUser from "./components/create-user.component";
 
 function App() {
   return (
     <Router>
       <div className='container'>
-      <NavbarMain />
+      <Navbar  />
       <br />
-      {/* <Route path='/elements' exact component = {ElementsList} /> */}
-      <Route path='/elements/create' exact component = {CreateElement} />
-      <Route path='/elements' exact component = {ElementsList} />     
-      {/* <Route path='/elements/edit/:id' exact component = {ManageElement} />       */} 
-      {/* <Route path='/user' exact component = {CreateUser} /> */}
+      <Route path='/' exact component = {ExercisesList} />
+      <Route path='/edit/:id' exact component = {EditExercise} />
+      <Route path='/create' exact component = {CreateExercise} />
+      <Route path='/user' exact component = {CreateUser} />
       </div>
     </Router>
   );
--- src/components/navbar.component.js
+++ src/components/navbar.component.js
@@ -1,73 +1,26 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import {Nav, Navbar, NavDropdown, Container} from 'react-bootstrap';
 
-import logo from "../logo.png";
-
-export default class NavbarMain extends Component {
+export default class Navbar extends Component {
 
   render() {
     return (
-
-
-      // <nav className="navbar navbar-light bg-light navbar-expand-lg">
-      //   <Link to="/" className="navbar-brand">
-      //     <img src={logo} alt='' width="200" height="40" className="d-inline-block align-top" />
-      //   </Link>
-      //   <div className="collpase navbar-collapse">
-      //   <ul className="navbar-nav mr-auto">
-      //     <li className="navbar-item dropdown" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-      //         <Link to="/" className="nav-link dropdown-toggle">Elements</Link>
-      //       <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-      //         <a className="dropdown-item" href="/">Action</a>
-      //         <a className="dropdown-item" href="/">Another action</a>
-      //         <a className="dropdown-item" href="/">Something else here</a>
-      //       </div>
-      //     </li>
-      //     <li className="navbar-item">
-      //     <Link to="/create" className="nav-link">Tracks</Link>
-      //     </li>
-      //     <li className="navbar-item">
-      //     <Link to="/" className="nav-link">Programs</Link>
-      //     </li>
-      //   </ul>
-      //   </div>
-      // </nav>
-
-
-      <Container>
-          <Navbar bg="light" expand="lg">
-            <Navbar.Brand href="/">
-              <img
-                alt=""
-                src={logo}
-                width="200"
-                height="40"
-                className="d-inline-block align-top"
-              />
-            </Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-              <Nav className="mr-auto">
-                {/* <Nav.Link href="/">Home</Nav.Link> */}
-                <NavDropdown title="Elements" id="element-dropdown">
-                  <NavDropdown.Item href="/elements/create">Create Element</NavDropdown.Item>
-                  <NavDropdown.Item href="/elements">Manage Elements</NavDropdown.Item>
-                </NavDropdown>
-                <NavDropdown title="Tracks" id="element-dropdown">
-                  <NavDropdown.Item href="/elements/create">Create Track</NavDropdown.Item>
-                  <NavDropdown.Item href="/elements/edit/:id">Edit Track</NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.3" disabled>Delete Track</NavDropdown.Item>
-                </NavDropdown>
-                <NavDropdown title="Programs" id="basic-nav-dropdown">
-                  <NavDropdown.Item href="#action/3.1"disabled>Create Program</NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.2"disabled>Edit Program</NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.3"disabled>View Program</NavDropdown.Item>
-                </NavDropdown>
-              </Nav>
-            </Navbar.Collapse>
-          </Navbar>
-        </Container>
+      <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
+        <Link to="/" className="navbar-brand">ExcerTracker</Link>
+        <div className="collpase navbar-collapse">
+        <ul className="navbar-nav mr-auto">
+          <li className="navbar-item">
+          <Link to="/" className="nav-link">Exercises</Link>
+          </li>
+          <li className="navbar-item">
+          <Link to="/create" className="nav-link">Create Exercise Log</Link>
+          </li>
+          <li className="navbar-item">
+          <Link to="/user" className="nav-link">Create User</Link>
+          </li>
+        </ul>
+        </div>
+      </nav>
     );
   }
 }
