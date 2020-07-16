dc3f8f51341cab264d09a9764fb20c36eaf8c754
SamsonPit12 <edressler6@gmail.com>
2020-07-16 15:39:03 -0400

    .

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "client/build")));
+app.use(express.static(path.join(__dirname, "/build")));
 
 mongoose.connect("mongodb://127.0.0.1:27017/powerbrain", {
   useNewUrlParser: true,
