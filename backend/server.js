6ca2d32acb06f008c40111dcd732bc284926949e
SamsonPit12 <edressler6@gmail.com>
2020-07-16 15:04:56 -0400

    .

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const path = require("path");
+
 
 require("dotenv").config();
 
@@ -9,6 +11,7 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "client/build")));
 
 mongoose.connect("mongodb://127.0.0.1:27017/powerbrain", {
   useNewUrlParser: true,
