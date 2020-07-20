3983ca43fe53956c72f2a7e9c6f37d6fa71d0f8a
SamsonPit12 <edressler6@gmail.com>
2020-07-20 12:45:05 -0400

    restructured folder

--- backend/models/element-dropdown-models/formatModel.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require("mongoose");
-const formatSchema = require("../../schemas/dropdowns/formatSchema");
-
-const Format = mongoose.model("Format", formatSchema);
-
-module.exports = Format;
